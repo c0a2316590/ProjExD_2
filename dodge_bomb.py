667bb269313f2b43e9db9bb0d4dd7960c823f752
c0a23165 <c0a2316590@edu.teu.ac.jp>
2024-11-26 16:55:05 +0900

    演習２：爆弾拡大、加速

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -30,29 +30,47 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
 
 
 def gameover(screen: pg.Surface) -> None:
+    """
+    GameOver時の画面の作成を行う
+    背景画面とGameOverの文字、こうかとん画像を表示
+    引数：screen
+    戻り値：なし
+    """
     # GameOver時の背景画面
     gameover_haikei = pg.Surface((WIDTH, HEIGHT))
     pg.draw.rect(gameover_haikei, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
     gameover_haikei.set_alpha(200)
     screen.blit(gameover_haikei, [0, 0])
-
     # GameOverの表示
     fonto = pg.font.Font(None, 80)
     txt = fonto.render("Game Over", True, (255, 255, 255))
     txt_rct = txt.get_rect()
     txt_rct.center = WIDTH/2, HEIGHT/2
     screen.blit(txt, txt_rct)
-
     # こうかとんの表示
     kk_img = pg.image.load("fig/8.png")
     kk_rct = kk_img.get_rect()
     kk_rct.center = WIDTH/2, HEIGHT/2
     screen.blit(kk_img, [kk_rct[0]-200, kk_rct[1]])
     screen.blit(kk_img, [kk_rct[0]+200, kk_rct[1]])
-
     pg.display.update()
     time.sleep(5)
 
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    時間とともに爆弾の速度と大きさが加速、拡大する
+    引数：なし
+    戻り値：拡大爆弾のリストと加速度のリスト
+    """
+    accs = [a for a in range(1, 11)]  # 加速度のリスト
+    bb_lst = []  # 拡大爆弾のリスト
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))  # 四隅の黒を透過
+        bb_lst.append(bb_img)  # 拡大爆弾のリストに追加
+    return bb_lst, accs
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -61,6 +79,7 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    vx, vy = +5, +5  # 爆弾速度ベクトル
     bb_img = pg.Surface((20, 20))  # 爆弾用の空surface
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 爆弾円を描く
     bb_img.set_colorkey((0, 0, 0))  # 四隅の黒を透過
@@ -89,7 +108,13 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)  # 爆弾動く
+        bb_imgs, bb_accs = init_bb_imgs()
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+        bb_rct.move_ip(avx, avy)  # 爆弾動く
         tate, yoko = check_bound(bb_rct)
         if not tate:
             vx *= -1
